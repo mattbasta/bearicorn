97a3cb0af6d74e8d841560f4745001cfc6d97f07
Matt Basta <me@mattbasta.com>
2013-08-16 10:57:11 -0700

    Fixes

--- script.js
+++ script.js
@@ -18,11 +18,13 @@ function push(scale, tx, ty) {
     ctx.scale(scale, scale);
     ctx.translate(percentx(tx), percenty(ty));
 }
-function pop() {ctx.restore();}
 function text(t, x, y) {ctx.fillText(t, x || 0, y || 0);}
 function percentx(x) {return x / 320 * width;}
 function percenty(x) {return x / 192 * height;}
-function random() {return Math.random();}
+var pop = function() {ctx.restore();};
+var random = Math.random;
+var abs = Math.abs;
+var min = Math.min;
 function noop() {};
 
 // To encode: btoa(String.fromCharCode.apply(String, [...]))
@@ -53,7 +55,7 @@ var drawMoose = sprite(5, 'green')(mcSprite("AQQBAQEFAgQDBQQCBQcCAgUEBAEGAQEDBwA
 var bearSprites = [
     mcSprite("AQUFAgAGAQMGBAICAgcEAQMBAwEEAAEBBQgBAQ=="),
     mcSprite("BwMBAQQCAQE="),
-    mcSprite("BAYBAQMHAQI="),
+    mcSprite("BAYBAQMHAQI=")
 ];
 var drawBear = sprite(3.5, 'brown')(function() {
     bearSprites[0]();
@@ -65,19 +67,18 @@ var drawBear = sprite(3.5, 'brown')(function() {
     bearSprites[2]();
 });
 
-function clear() {ctx.clearRect(0, 0, width, height);}
-
 // State Vars
 var mticks = 0;
 var message;
 // 1
-var x, y, vy;
+var x, y = 0, vy = 0;
 var moose_x, moose_speed;
 var rocks;
 var holes;
 var beerpos;
 var difficulty = 0;
 var stage;
+var ground_top = height * 0.92;
 // 2
 // 3
 // 4
@@ -95,33 +96,32 @@ var states = [
         color('blue');
         fr(0, 0, width, height);
         color('#fff');
-        push(4, 10, 25);
+        push(4, width / 8 - 32, height / 4 * 0.5);
         text('BEARICORN');
         pop();
-        text('PRESS ARROW KEY TO START', percentx(80), percenty(130));
+        text('PRESS ARROW KEY TO START', width / 2 - 80, height * 0.6);
     },
     // 1 - Level 1
     function(ratio) {
-        clear();
         // Rendering
         color('black');
         fill();
         color('gray');
         for (var i = 0, r; r = rocks[i++];) {
             function rock_reset() {
-                r[0] = random() * 6 * 40 + 40;
+                r[0] = (random() * 0.8 + 0.2) * width;
                 r[1] = 0;
             }
-            fr(percentx(r[0]), percenty(r[1]), 10, 10);
+            fr(r[0], r[1], 10, 10);
             r[1] += ratio * difficulty * 0.3;
             if (r[1] > height) {
                 rock_reset();
             }
             // Some game logic (shhh)
-            if (Math.abs(r[1] - y - 155) < 15 &&
-                Math.abs(r[0] - x) < 15) {
+            if (abs(r[1] - y - 155) < 15 &&
+                abs(r[0] - x) < 15) {
                 if (vy < 0) {
-                    vy = Math.min(vy + 12, 7);
+                    vy = min(vy + 12, 7);
                     rock_reset();
                 } else {
                     return setups[state = 5]();
@@ -132,19 +132,19 @@ var states = [
         if (moose_x < 100)
             drawMoose(240 + (moose_x += moose_speed += 0.25), 132);
         color('brown');
-        fr(0, percenty(177), percentx(40), percenty(192));
-        fr(percentx(280), percenty(177), percentx(320), percenty(192));
+        fr(0, ground_top, width * 0.1 + 1, height);
+        fr(width * 0.9, ground_top, width * 0.1, height);
 
-        for(var i = 0; i < 6; i++)
+        for(var i = 0; i < 6; i++) {
             if(!holes[i])
-                fr(percentx((i+1) * 40), percenty(177), percentx(40), percenty(40));
+                fr(width * (i * 0.133 + 0.1) + 1, ground_top, width * 0.133 + 1, height);
+        }
 
-        var bp = beerpos + 1;
-        if (bp) {
+        if (beerpos + 1) {
             color('blue');
-            var bx = bp * 40 + 15;
-            fr(percentx(bx), percenty(162), percentx(10), percenty(15));
-            if (Math.abs(x + 5 - bx) < 10 && y > -10) {
+            var bx = (beerpos * 0.133 + 0.15) * width;
+            fr(bx, ground_top - 20, width * 0.03, 20);
+            if (abs(x + 5 - bx) < 10 && y > -10) {
                 difficulty /= 2;
                 beerpos = -1;
                 mticks = 100;
@@ -154,7 +154,15 @@ var states = [
 
         // Game logic
         function get_floor(new_x) {
-            return (new_x > 30 && new_x < 270 && holes[(new_x - 30) / 40 | 0]) ? 100 : 0;
+            // TODO: This can be heavily optimized.
+            var px = new_x / width;
+            if (px > 0.1 && px < 0.8) {
+                px -= 0.1;
+                return min(holes[px / 0.133 | 0],
+                           holes[(px + 0.08) / 0.133 | 0]) ? 100 : 0;
+            } else {
+                return 0;
+            }
         }
         var floor = get_floor(x);
         if (keys.jump) {
@@ -169,7 +177,8 @@ var states = [
         }
         
         if (y > 50) {
-            return setups[state = (random() * 10 | 0) == 0 ? 7 : 5]();
+            return setups[state = random() * 10 | 0 ? 5 : 7]();
+            // return setups[state = 7]();
         }
 
         var xtemp = x;
@@ -183,7 +192,7 @@ var states = [
 
         if (mticks > 0) {
             color('#fff');
-            push(4, percentx(20), percenty(20 - Math.abs(Math.sin((mticks -= ratio) / 10)) * 5));
+            push(4, percentx(20), percenty(20 - abs(Math.sin((mticks -= ratio) / 10)) * 5));
             text(message);
             pop();
         }
@@ -214,14 +223,7 @@ var states = [
         }
     },
     // 7 - ET
-    function() {
-        color('brown');
-        fill();
-        color('#000');
-        fr(percentx(100), 0, percentx(110), percenty(160));
-        drawBear(110, 130);
-        drawET(165, 130);
-    }
+    noop
 ];
 var setups = [
     // 0 - Start screen
@@ -236,11 +238,11 @@ var setups = [
             stage = 0;
         } else {
             difficulty++;
-            if (rocks.length < difficulty / 2 | 0) {
+            if (rocks.length * 2 < difficulty) {
                 rocks.push([]);
             }
             // Put the beer on a random pillar.
-            beerpos = (random() * 3 | 0) == 0 ? random() * 4 | 0 : -1;
+            beerpos = random() * 3 | 0 ? -1 : random() * 6 | 0;
             stage++;
         }
         // Set up the moose
@@ -248,19 +250,19 @@ var setups = [
         moose_speed = 0.5;
         
         // Set up holes
-        holes = [0, 0, 0, 0, 0, 0];
-        for (var i = 0; i < Math.min(4, difficulty * 0.15 + 1); i++) {
+        holes = Array(6);
+        for (var i = 0; i < min(4, difficulty * 0.15 + 1); i++) {
             var hole;
             holes[hole = random() * 6 | 0] = 1;
             if (hole == beerpos) {beerpos = -1;}
         }
         // Set up the rocks
         for (var i = 0, r; r = rocks[i++];) {
-            r[0] = random() * 6 * 40 + 10;
-            r[1] = random() * 150 - 75;
+            r[0] = (random() * 0.85 + 0.1) * width;
+            r[1] = (random() - 0.3) * height;
         }
         // Reset the state vars
-        x = y = vy = since_jump = 0;
+        x = since_jump = 0;
     },
     // 2 - Level 2
     noop,
@@ -270,10 +272,12 @@ var setups = [
     noop,
     // 5 - Losing
     function() {
+        vy = 0;
         death_count = 0;
     },
     // 6 - Lost
     function() {
+        y = 0;
         death_count = 0;
         color('#000');
         fill();
@@ -283,7 +287,20 @@ var setups = [
         pop();
     },
     // 7 - ET
-    noop
+    function() {
+        color('brown');
+        fill();
+        color('#000');
+        fr(width / 3, 0, width / 3, ground_top);
+        drawBear(110, 145);
+        if (random() * 200 | 0) {
+        // if (0) {
+            drawET(165, 145);
+        } else {
+            ctx.scale(-1, 1);
+            drawMoose(-210, 132);
+        }
+    }
 ];
 
 var lastTime = (new Date()).getTime();
@@ -304,19 +321,15 @@ raf(loop);
 
 ael('keydown', key);
 ael('keyup', key);
+var keybindings = {38: 'jump', 37: 'left', 39: 'right'}
 function key(e, enable) {
-    //log("Key: " + e.keyCode + " " + enable);
-    var kc = e.keyCode;
     var enable = e.type == 'keydown';
-    if(state == 0) {
+    var kc = e.keyCode;
+    if(!state) {
         // If an arrow key was pressed on the start screen, run the setup for state 1.
         return (kc > 39 || kc < 37) ? 0 : setups[state = 1]();
     }
-    switch(kc) {
-        case 38: return keys.jump = enable;
-        case 37: return keys.left = enable;
-        case 39: return keys.right = enable;
-    }
+    keys[keybindings[kc]] = enable;
 };
 
 })(canvas.getContext('2d'), canvas.width, canvas.height);
